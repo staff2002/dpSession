bae7c53f4793c6542c84e873d81cb37d95b68d9d
LiYiming <yiming.li01@ninebot.com>
2025-09-03 09:44:17 +0800

    refactor

--- src/main/java/org/dp/simplefactory/v3/Client.java
+++ src/main/java/org/dp/simplefactory/v3/Client.java
@@ -1,13 +1,12 @@
 package org.dp.simplefactory.v3;
 
-import org.dp.simplefactory.v2.ProductRecommenderV2;
-
 import java.util.List;
 
 public class Client {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         ProductRecommenderV3 productRecommender = new ProductRecommenderV3();
-        List<String> products = productRecommender.recommend("TV");
+        List<String> products = null;
+        products = productRecommender.recommend("television");
         products.forEach(System.out::println);
     }
 }
--- src/main/java/org/dp/simplefactory/v4/Client.java
+++ src/main/java/org/dp/simplefactory/v4/Client.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Client {
     public static void main(String[] args) throws Exception {
         ProductRecommenderV4 productRecommender = new ProductRecommenderV4();
-        List<String> products = productRecommender.recommend("TV");
+        List<String> products = productRecommender.recommend("television");
         products.forEach(System.out::println);
     }
 }
